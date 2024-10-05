066200b6dd8694e3d2f2eaf79979ad3bf056413a
Betsabe Iglesias Marques <beiglesi@c3r6s6.42urduliz.com>
2024-10-05 14:13:02 +0200

    prompt subiendo

--- prompt.c
+++ prompt.c
@@ -6,13 +6,39 @@
 /*   By: beiglesi <beiglesi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 11:31:21 by beiglesi          #+#    #+#             */
-/*   Updated: 2024/10/05 11:33:10 by beiglesi         ###   ########.fr       */
+/*   Updated: 2024/10/05 13:30:05 by beiglesi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-void ft_prompt()
+char *prompt(void)
 {
-    printf("esto es el prompt\n");
+	char *str;
+
+    str = readline("minishell> ");
+	add_history(str);
+        
+    return (str);
+    
+}
+
+int main()
+{
+	char *line;
+
+	while(1)
+	{	
+		line = prompt( );
+		free(line);
+		if (line == NULL) // Detecta EOF (Ctrl+D)
+    		break;
+
+	}
+	
+	return(0);
 }
