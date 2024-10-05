763605a50831a5692b59e7468999be0dd69b3b3a
Aritz Olabarriaga Santamaria <aolabarr@c3r7s5.42urduliz.com>
2024-10-05 11:43:42 +0200

    pruebas01

--- prueba_signals.c
+++ prueba_signals.c
@@ -16,25 +16,27 @@ void ft_prompt()
 
 void own_function(void)
 {
-	char buffer[1000];
-	printf("executing OWN_FUNCTION\n");
-	read(0, buffer, BUFFER_SIZE);
+	//static char buffer[1000];
+	printf("\nexecuting OWN_FUNCTION\n");
+	//read(0, buffer, BUFFER_SIZE);
 }
 
 void exe_c(int num)
 {
-    printf("Escribe CNTRL+C, num: %d\n", num);
+	num = num;
+    //printf("Escribe CNTRL+C, num: %d\n", num);
 }
 void exe_d(int num)
 {
-    printf("Escribe CNTRL+D, num: %d\n", num);
+	num = num;
+    //printf("Escribe CNTRL+\, num: %d\n", num);
 }
 
 int main()
 {
 	int a = 1;
     signal(SIGINT, &exe_c);
-	signal(SIGQUIT, &exe_d);
+	//signal(SIGQUIT, &exe_d);
 	while(1)
 	{
 		if (a == 1)
@@ -47,3 +49,4 @@ int main()
 	}
     return (0);
 }
+///////////
