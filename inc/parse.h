075af9b2fd64ba609e61a0bff83ac0bbcaeed92d
Aritz Olabarriaga Santamaria <aolabarr@c3r7s5.42urduliz.com>
2024-10-05 18:20:21 +0200

    reglas de sintaxis en header

--- inc/parse.h
+++ inc/parse.h
@@ -6,7 +6,7 @@
 /*   By: aolabarr <aolabarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:39:06 by beiglesi          #+#    #+#             */
-/*   Updated: 2024/10/05 17:01:52 by aolabarr         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:19:03 by aolabarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define PARSE_H
 
 bool	check_quotes(char *str);
+// si solo hay espacios blanco o tabulaciones etc           	-> no hacer nada
+// si el primer caracter despues de los espacios 
+//						no es [A-Z] o [a-z] o $ -> mensaje 		-> syntax error
+// si el ultimo caracter es pipe, redirecciones, metacaracteres	-> syntax error
+
 
 #endif
