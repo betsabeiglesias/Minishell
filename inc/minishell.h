068aed4efb3d1115754fa58cf481fbc42779685f
Aritz Olabarriaga Santamaria <aolabarr@c3r5s4.42urduliz.com>
2024-10-13 15:10:13 +0200

    Creado struct de nodos token en minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,12 +6,37 @@
 /*   By: aolabarr <aolabarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 13:13:15 by aolabarr          #+#    #+#             */
-/*   Updated: 2024/10/05 16:44:54 by aolabarr         ###   ########.fr       */
+/*   Updated: 2024/10/13 15:05:48 by aolabarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+typedef enum    e_token_type
+{
+    CMD,
+    PIPE,
+    DQ_STR,
+    SQ_STR,
+    RDS_IN,
+    RDS_OUT,
+    RDD_IN,
+    RDD_OUT,
+    EXIT_CODE,
+    NO_TYPE
+}				t_token_type;
+
+
+typedef struct s_token
+{
+   t_token_type  type;
+   char         *cmd;
+   char			**cmd_av;
+   char			*str;
+   t_token      *next;
+   t_token      *prev;
+}				t_token;
 
 # include <unistd.h>
 # include <stdlib.h>
--- src/tokenization.c
+++ src/tokenization.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_error.c                                     :+:      :+:    :+:   */
+/*   tokenization.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aolabarr <aolabarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/10 16:43:26 by aolabarr          #+#    #+#             */
-/*   Updated: 2024/10/13 15:09:02 by aolabarr         ###   ########.fr       */
+/*   Created: 2024/10/13 15:06:55 by aolabarr          #+#    #+#             */
+/*   Updated: 2024/10/13 15:08:51 by aolabarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	handle_error(int error_type)
-{
-	if (error_type == ERR_QUOTE)
-		ft_putendl_fd(ERR_MSG_QUOTE, STDOUT_FILENO);
-	if (error_type == ERR_EXTREM)
-		ft_putendl_fd(ERR_MSG_EXTREM, STDOUT_FILENO);
-	if (error_type == ERR_ALONE)
-		ft_putendl_fd(ERR_MSG_ALONE, STDOUT_FILENO);
-	return (1);
-}
