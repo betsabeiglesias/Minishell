dfdf6e46703414dc5dea007a75fcba72eb5b4e8c
Aritz Olabarriaga Santamaria <aolabarr@c3r5s4.42urduliz.com>
2024-11-30 13:06:32 +0100

    procesos con fork funcionan bien, programar builtin unitarios

--- src/execution_1.c
+++ src/execution_1.c
@@ -6,7 +6,7 @@
 /*   By: aolabarr <aolabarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 18:56:38 by aolabarr          #+#    #+#             */
-/*   Updated: 2024/11/30 12:18:37 by aolabarr         ###   ########.fr       */
+/*   Updated: 2024/11/30 13:05:37 by aolabarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,7 @@ int	init_execution(t_list *exe_lst, t_mini *shell)
 	i = 0;
 	while (i < num_procs)
 	{
-		if (is_builtin(((t_exec *)exe_lst->content)->cmd_all[0]))
-		{
-			if (do_redirections((t_exec *)exe_lst->content, i, num_procs, shell))
-				return (EXIT_FAILURE);
-			execute_builtin((t_exec *)exe_lst->content, shell);
-			//return (EXIT_SUCCESS);
-		}
-		else
+		
 		{
 			shell->pid[i] = fork();
 			if (shell->pid[i] == ERROR)
@@ -60,40 +53,17 @@ int	init_execution(t_list *exe_lst, t_mini *shell)
 
 int exe_child(t_exec *node, int child, int num_procs, t_mini *shell)
 {
-	/*
-    int fd_in;
-    int fd_out;
-
-	fd_in = 0;
-	fd_out = 0;
-	if (child != 0)
-		dup2(shell->pipes[child - 1][RD_END], STDIN_FILENO);
-	if (child != num_procs - 1)
-		dup2(shell->pipes[child][WR_END], STDOUT_FILENO);
-    if (node->filename_in != NULL)
-    {
-        fd_in = open(node->filename_in, O_RDONLY);
-        if (fd_in == ERROR)
-		    return (handle_error(ERR_OPEN), EXIT_FAILURE);
-    }
-    if (node->filename_out != NULL && node->out_append == 0)
-        fd_out =  open(node->filename_out, O_CREAT | O_RDWR | O_TRUNC, 0644);
-    else if (node->filename_out != NULL && node->out_append == 1)
-        fd_out = open(node->filename_out, O_CREAT | O_RDWR | O_APPEND, 0644);
-    if (fd_out == ERROR)
-		return (handle_error(ERR_OPEN), EXIT_FAILURE);
-    if (node->filename_in != NULL)
-        dup2(fd_in, STDIN_FILENO);
-	 if (node->filename_out != NULL)
-        dup2(fd_out, STDOUT_FILENO);
-    close_pipes(shell, num_procs);
-	*/
 	if (do_redirections(node, child, num_procs, shell))
 		return (EXIT_FAILURE);
-	execve(node->path, node->cmd_all, shell->env);
+	if (is_builtin((node->cmd_all[0])))
+	{
+		execute_builtin(node, shell);
+		exit(EXIT_SUCCESS);
+	}
+	else
+		execve(node->path, node->cmd_all, shell->env);
 	handle_error(ERR_EXECVE);
-
-    return (EXIT_SUCCESS);
+    return (EXIT_FAILURE);
 }
 
 int	wait_childs(t_mini *shell, int num_procs)
@@ -184,9 +154,9 @@ int do_redirections(t_exec *node, int child, int num_procs, t_mini *shell)
 		    return (handle_error(ERR_OPEN), EXIT_FAILURE);
     }
     if (node->filename_out != NULL && node->out_append == 0)
-        fd_out =  open(node->filename_out, O_CREAT | O_RDWR | O_TRUNC, 0644);
+        fd_out =  open(node->filename_out, O_CREAT | O_RDWR | O_TRUNC, 644);
     else if (node->filename_out != NULL && node->out_append == 1)
-        fd_out = open(node->filename_out, O_CREAT | O_RDWR | O_APPEND, 0644);
+        fd_out = open(node->filename_out, O_CREAT | O_RDWR | O_APPEND, 644);
     if (fd_out == ERROR)
 		return (handle_error(ERR_OPEN), EXIT_FAILURE);
     if (node->filename_in != NULL)
