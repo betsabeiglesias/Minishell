7a32b10b1ee8cf1912e321016320d41ccfe29a4f
Betsabe Iglesias <biglesiasmarques@gmail.com>
2024-12-15 17:03:35 +0100

    arreglado algún leak

--- src/execution_1.c
+++ src/execution_1.c
@@ -6,7 +6,7 @@
 /*   By: binary <binary@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2024/12/16 16:10:22 by binary           ###   ########.fr       */
+/*   Updated: 2024/12/16 16:12:36 by binary           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,10 @@ int	init_execution(t_list *exe_lst, t_mini *shell)
 			}
 			i++;
 			exe_lst = exe_lst->next;
+		}
+	}
+	close_pipes(shell, num_procs);
+	if (shell->pid != NULL)
 		}
 	}
 	close_pipes(shell, num_procs);
